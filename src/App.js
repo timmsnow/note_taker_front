1375e5a229efb4310ebcbbc68be4419fc6da9832
TIMM SNOW <two.tone.snow@gmail.com>
2021-08-04 11:44:44 -0400

    adds axios modules and home + notes component, writes sample axios request to be modified later

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Switch, Route, withRouter } from "react-router-dom";
+import Home from "./components/home";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
- <div className="main">
+      <div className="main">
     {/* <Navigation/> */}
     <Main />
     </div>
@@ -13,9 +15,16 @@ function App() {
   );
 }
 
+const AppWithRouter = withRouter(App);
+
 const Main = () => (
-<div>
-  <h1>Notes!</h1>
-</div>
+<Switch>
+    <Route exact path="/" component={Home}></Route>
+    {/* <Route exact path="/login" component={Login}></Route>
+    <Route exact path="/dashboard" component={Dashboard}></Route>
+    <Route exact path="/logout" component={Logout}></Route>
+    <Route exact path="/contacts" component={Contacts}></Route>
+  <Route exact path="/about" component={About}></Route> */}
+  </Switch>
 );
-export default App;
+export default AppWithRouter;
---
+++ src/components/home.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import axios from 'axios';
+
+class Home extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+    errors: [],
+    users: [],
+  };
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers = () => {
+    axios.get("http://localhost:3000/users").then((response) => {
+      console.log(response.data);
+      const users = response.data;
+        this.setState({ users });
+    });
+  };
+
+  render() { 
+    return ( 
+    <div>
+    <h1>Notes!</h1>
+    <ul>
+        { this.state.users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </div>
+     );
+  }
+}
+ 
+
+export default Home;
